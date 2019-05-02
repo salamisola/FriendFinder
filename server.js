ae7025408445efc97c6804214c711bdc1678084f
solasalami <salamisola@gmail.com>
2019-05-02 17:55:43 -0400

    home.html and survey.html files added and updated

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Pull in required dependencies
+// setup in required dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
@@ -20,36 +20,8 @@ require(path.join(__dirname, './app/routing/apiRoutes2'))(app);
 require(path.join(__dirname, './app/routing/htmlRoutes2'))(app);
 
 // Start listening on PORT
-app.listen(PORT, function() {
-  console.log('Friend Finder app is listening on PORT: ' + PORT);
+app.listen(PORT, function () {
+    console.log('FriendFinderApp is listening on PORT: ' + PORT);
 });
 
 
-// Dependencies
-// =============================================================
-/*var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var fs = require('fs');
-
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-
-// Static files
-app.use(express.static('app/public'));
-
-
-// Router
-require('./app/routing/apiRoutes.js')(app, path);
-require('./app/routing/htmlRoutes.js')(app, path);
-
-
-app.listen(PORT, function() {
-    console.log("I\'m listening... on port " + PORT);
-})*/
