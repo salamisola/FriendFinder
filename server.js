4a98f15d07323b2b741bb2ac0f00bb08070f56c4
solasalami <salamisola@gmail.com>
2019-05-03 00:31:49 -0400

    server.js updated

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
-// setup in required dependencies
+/*var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+app.use(express.static("app/public"));
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+app.listen(port, ()=>console.log("Listening on port %s", port));*/
+
+
+
+
+// Pull in required dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
 // Configure the Express application
 var app = express();
-var PORT = process.env.PORT ||
+var PORT = process.env.PORT || 3000;
 
-    // Expose the public directory to access CSS files
-    app.use(express.static(path.join(__dirname, './app/public')));
+// Expose the public directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
 
 // Add middleware for parsing incoming request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text({ tyoe: 'text/html' }));
+app.use(bodyParser.text());
 
 // Add the application routes
-require(path.join(__dirname, './app/routing/apiRoutes2'))(app);
-require(path.join(__dirname, './app/routing/htmlRoutes2'))(app);
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 // Start listening on PORT
 app.listen(PORT, function () {
-    console.log('FriendFinderApp is listening on PORT: ' + PORT);
+  console.log("Friend Finder app is listening on PORT: " + PORT);
 });
 
-
+/*app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});*/
